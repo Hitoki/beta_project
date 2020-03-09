f017c6e7ecea77d8e0e805deb6bcd9da6d2ec58a
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2020-03-09 16:30:36 +0200

    changing according to pr issues

--- rent_cars/models.py
+++ rent_cars/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    first_name = models.CharField(max_length=128)
-    second_name = models.CharField(max_length=128, null=True)
-    age = models.IntegerField()
-    auto = models.ForeignKey("Auto", on_delete=models.SET_NULL, blank=True, null=True)
-    rent_time = models.DateTimeField()
-
-
 class Auto(models.Model):
     year = models.IntegerField()
     color = models.CharField(max_length=128)
@@ -20,3 +12,12 @@ class Brand(models.Model):
     name = models.CharField(max_length=128)
     category = models.CharField(max_length=128)
     country = models.CharField(max_length=128)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128, blank=True, default="")
+    dateOfBirth = models.DateTimeField()
+    auto = models.ManyToManyField(Auto)
+    rent_time = models.DateTimeField()
+
