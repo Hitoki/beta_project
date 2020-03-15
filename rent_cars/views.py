e6ab13b36dde226261f271ff42b9491339d3315a
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2020-03-15 14:09:57 +0200

    added templates

--- rent_cars/views.py
+++ rent_cars/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return HttpResponse("The rent cars app will be here!")
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class AutoView(TemplateView):
+    template_name = "auto.html"
+
+
+class FeedView(TemplateView):
+    template_name = "feed.html"
+
+
+class PostView(TemplateView):
+    template_name = "post.html"
