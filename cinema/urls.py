80da634f4789379c87f9e56475149ea782dae807
YuriiBorovenskyi <yurii.borovenskyi@gmail.com>
2020-03-30 13:15:45 +0300

    Added / update next pathes in cinema/urls.py: 'blu-ray/', 'blu-ray/<int:pk>/', 'news/', 'news/<int:pk>/', 'film/<int:pk>/', 'movie-person/<int:pk>/'. Also added / updated views in cinema/views.py corresponding to these paths.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 
-from .views import CinemaTemplateView, ProductDetailTemplateView, \
-    NewsListTemplateView, NewsDetailTemplateView
+from .views import ProductListView, ProductDetailView, \
+    NewsListView, NewsDetailView, FilmDetailView, CinemaPersonDetailView
 from .apps import CinemaConfig
 
 app_name = CinemaConfig.name
 
 urlpatterns = [
-    path('', CinemaTemplateView.as_view(), name='index'),
-    path('<int:pk>/', ProductDetailTemplateView.as_view(),
+    path('blu-ray/', ProductListView.as_view(), name='product-list'),
+    path('news/', NewsListView.as_view(), name='news-list'),
+    path('blu-ray/<int:pk>/', ProductDetailView.as_view(),
          name='product-detail'),
-    path('news/', NewsListTemplateView.as_view(), name='news-list'),
-    path('news/<int:pk>/', NewsDetailTemplateView.as_view(),
+    path('film/<int:pk>/', FilmDetailView.as_view(),
+         name='film-detail'),
+    path('movie-person/<int:pk>/', CinemaPersonDetailView.as_view(),
+         name='movie-person-detail'),
+    path('news/<int:pk>/', NewsDetailView.as_view(),
          name='news-detail'),
 ]
