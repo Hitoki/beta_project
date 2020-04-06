08997a1cc9e7714c00fb99aa89d5504352d19990
Dmytro Tvoretc <rus.tvoretc@gmail.com>
2020-04-06 22:20:05 +0300

    moved my static and media settings to local

--- beta_project/settings.py
+++ beta_project/settings.py
@@ -104,11 +104,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
