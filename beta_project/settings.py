aa2d74e7bc84a4f155b2fb592c5b910ee7a80e17
YuriiBorovenskyi <yurii.borovenskyi@gmail.com>
2020-04-15 00:56:28 +0300

    Added / updated some media information about films, persons and news.

--- beta_project/settings.py
+++ beta_project/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # Students` applications
     'insane_app',
